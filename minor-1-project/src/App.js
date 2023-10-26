a17ef0ccc4d0723e2922e7e9510ec06de84b6c61
aakshitaa <aakshitasingh786@gmail.com>
2023-10-26 12:44:17 +0530

    events page added

--- minor-1-project/src/App.js
+++ minor-1-project/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
-
+import About from './Components/Events/Events'
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <About />
     </Router>
   );
 }
---
+++ minor-1-project/src/Components/Events/Events.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Events.css'
+export default function AboutUs(){
+    return(
+        <div class="pic-ctn">
+    <img src="https://picsum.photos/200/300?t=1" alt="" class="pic"/>
+    <img src="https://picsum.photos/200/300?t=2" alt="" class="pic"/>
+    <img src="https://picsum.photos/200/300?t=3" alt="" class="pic"/>
+    <img src="https://picsum.photos/200/300?t=4" alt="" class="pic"/>
+    <img src="https://picsum.photos/200/300?t=5" alt="" class="pic"/>
+  </div>
+    );
+};
