11d4901d18ff3d6f9faff6747a2bdecb0d380d51
shwetasng <shwetasng17@gmail.com>
2023-10-28 11:54:59 +0530

    AboutUs section added

--- minor-1-project/src/App.js
+++ minor-1-project/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
-import About from './Components/Events/Events'
+import About from './Components/About/AboutUs'
+import Events from './Components/Events/Events'
 import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <Router>
-      <Navbar />
       <About />
-      <Footer />
+      {/* <Navbar />
+      <Events />
+      <Footer /> */}
     </Router>
   );
 }
---
+++ minor-1-project/src/Components/About/AboutUs.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './AboutUs.css'
+
+
+
+const AboutUs = (props) => {
+    return (
+      <div className={`about-us ${props.theme === 'dark' ? 'dark-mode' : ''}`}>
+        <section className={`about-us ${props.theme === 'dark' ? 'dark-mode' : ''}`} style={{fontFamily:"'Poppins', sans-serif"}}>
+          <div className="about">
+          {/* <img src={} alt="About Us" className="pic" /> */}
+            <div className="text">
+              <h2 className="heading" style={{color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`}}>About Us</h2>
+               <h5 style={{color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`}}>At our <span>Lost &amp;Found</span></h5>
+              <p className="paratext" style={{color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`}}>web app, we are dedicated to reuniting people with their lost belongings and restoring what's been found. We understand the frustration and anxiety that comes with losing something valuable, whether it's a sentimental item or a daily essential.
+              </p>
+              <div className="data">
+              <a href="/items" className="hire">Try Us</a>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  };
+  export default AboutUs;
