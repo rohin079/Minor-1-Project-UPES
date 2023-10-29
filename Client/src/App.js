5e02bb379338e01177a5bb4d2f4cd050b2e2b1f6
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2023-10-29 21:37:05 +0530

    changed project structure

---
+++ Client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home'
+// import About from './Components/About/AboutUs'
+// import Events from './Components/Events/Events'
+// import Footer from './Components/Footer/Footer';
+function App() {
+  return (
+    <Router>
+      <Home />
+      {/* <About /> */}
+      {/* <Navbar />
+      <Events />
+      <Footer /> */}
+    </Router>
+  );
+}
+
+export default App;
