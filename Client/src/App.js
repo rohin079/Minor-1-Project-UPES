e8afe4653663a0ba143783b6bdbae5c8b69a31d3
aakshitaa <aakshitasingh786@gmail.com>
2023-10-31 13:33:21 +0530

    feedback page fixed

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,11 +5,6 @@ import Home from './Components/Home/Home'
 import About from './Components/About/AboutUs'
 import Events from './Components/Events/Events'
 import { BrowserRouter as Router } from 'react-router-dom';
-// import Navbar from './Components/Navbar/Navbar';
-// import Home from './Components/Home/Home'
-// import About from './Components/About/AboutUs'
-// import Events from './Components/Events/Events'
-// import Footer from './Components/Footer/Footer';
 import Feedback from './Components/Feedback/FeedbackForm'
 
 
