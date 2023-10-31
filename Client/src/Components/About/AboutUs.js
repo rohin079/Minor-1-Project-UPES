1b14394d1712e3b5c33ef626b7581339314f0621
shwetasng <shwetasng17@gmail.com>
2023-11-01 02:02:34 +0530

    modified AboutUs page

--- Client/src/Components/About/AboutUs.js
+++ Client/src/Components/About/AboutUs.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './AboutUs.css';
-import Images from "../Images/about-pic.jpg";
+import Images from "./about1.png";
 
 
 
@@ -12,11 +12,11 @@ const AboutUs = (props) => {
           <img src={Images} alt="About Us" className="pic" />
             <div className="text">
               <h2 className="heading" style={{color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`}}>About Us</h2>
-               <h5 style={{color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`}}>At our <span>Lost &amp;Found</span></h5>
-              <p className="paratext" style={{color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`}}>web app, we are dedicated to reuniting people with their lost belongings and restoring what's been found. We understand the frustration and anxiety that comes with losing something valuable, whether it's a sentimental item or a daily essential.
+               <h5 style={{color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`}}>At our <span>uni-Connect</span></h5>
+              <p className="paratext" style={{color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`}}>"Hello, we are a group of enthusiastic third-year students currently pursuing our Bachelor of Technology in Computer Science and Engineering at UPES. With a shared passion for DevOps, we embarked on an exciting journey to create Uni-Connect, a platform designed to bridge the gap between students and faculty, making educational resources readily accessible. Our platform is more than just a repository; it's a hub for information on all college events, bringing the campus experience to your fingertips with a single click. We understand the value of mentorship, which is why we're dedicated to offering you the opportunity to hear from esteemed alumni, connecting the past with the present. At Uni-Connect, we're not just building a platform; we're shaping a community where learning, sharing, and networking thrive."
               </p>
               <div className="data">
-              <a href="/items" className="hire">Try Us</a>
+              <a href="/login" className="hire">Try Us</a>
               </div>
             </div>
           </div>
